049a82a4f2a248b635915cd19a285b5ec82c16db
marcusguedes2 <marcusguedesdrumond@gmail.com>
2024-06-06 19:34:38 -0300

    .

--- Controle_de_Estoque/src/main/java/com/mycompany/controle_de_estoque/endereco_carga.java
+++ Controle_de_Estoque/src/main/java/com/mycompany/controle_de_estoque/endereco_carga.java
@@ -87,19 +87,19 @@ public class endereco_carga extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Endereço", "Corredor", "Rua", "Pilha", "Nível"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(150, 90, 610, 310);
+        jScrollPane1.setBounds(150, 90, 670, 310);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
